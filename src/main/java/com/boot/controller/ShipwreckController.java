8e552bbe0afbd13e1ad6de84d310026077555787
Alexandru Peptan <alexandru.peptan.85@gmail.com>
2017-03-30 17:06:42 +0300

    Added Shipwreck model, ShipwreckController with RESTful endpoints and ShipwreckStub with initial test data. Added Angular static Frontend that can now talk with Spring MVC.

---
+++ src/main/java/com/boot/controller/ShipwreckController.java
@@ -0,0 +1,35 @@
+package com.boot.controller;
+
+import com.boot.model.Shipwreck;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by apeptan on 3/30/2017.
+ */
+@RestController
+@RequestMapping("api/v1/")
+public class ShipwreckController {
+
+    @RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
+    public List<Shipwreck> list(){
+        return ShipwreckStub.list();
+    }
+
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.GET)
+    public Shipwreck get(@PathVariable Long id){
+        return ShipwreckStub.get(id);
+    }
+
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.PUT)
+    public Shipwreck update(@PathVariable Long id, @RequestBody Shipwreck shipwreck){
+        return ShipwreckStub.update(id, shipwreck);
+    }
+
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.DELETE)
+    public Shipwreck delete(@PathVariable Long id){
+        return ShipwreckStub.delete(id);
+    }
+
+}
